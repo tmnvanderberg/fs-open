e8acc75f5ce0082a557ce6e12fc4900ac4b3b7a7
Timon van der Berg <tmnvanderberg@gmail.com>
2023-10-08 22:07:40 +0200

    1.6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,22 +1,32 @@
 import { useState } from "react";
 
-const Display = ({counter}) => <div> {counter} </div>
+const increment = (value, setter) => () => {
+  setter(value + 1);
+};
 
-const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const FeedbackButton = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="increase by one" />
-      <Button handleClick={decreaseByOne} text="decrease by one" />
-      <Button handleClick={setToZero} text="set to zero" />
+      <h1>give feedback</h1>
+      <FeedbackButton text="good" handleClick={increment(good, setGood)} />
+      <FeedbackButton
+        text="neutral"
+        handleClick={increment(neutral, setNeutral)}
+      />
+      <FeedbackButton text="bad" handleClick={increment(bad, setBad)} />
+      <h1>statistics</h1>
+      <div> good {good} </div>
+      <div> neutral {neutral} </div>
+      <div> bad {bad} </div>
     </div>
   );
 };
