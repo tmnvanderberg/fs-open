a352c1d7e65d8d90f18e3baae55c31c23c8b6093
Timon van der Berg <tmnvanderberg@gmail.com>
2023-10-10 22:09:56 +0200

    1.12

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,51 +1,32 @@
 import { useState } from "react";
 
-const increment = (value, setter) => () => {
-  setter(value + 1);
-};
-
-const Button = ({ text, handleClick }) => {
-  return <button onClick={handleClick}>{text}</button>;
-};
-
-const StatisticLine = ({ name, value }) => {
-  return (
-    <tr>
-      <td>{name}</td> 
-      <td>{value}</td>
-    </tr>
-  );
-};
-
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad;
-  const positive = all ? good / all : 0;
-  return all ? (
-    <table>
-      <StatisticLine name="good" value={good} />
-      <StatisticLine name="neutral" value={neutral} />
-      <StatisticLine name="bad" value={bad} />
-      <StatisticLine name="all" value={all} />
-      <StatisticLine name="average" value={(good - bad) / all} />
-      <StatisticLine name="positive" value={positive + " %"} />
-    </table>
-  ) : (
-    <div> No feedback given </div>
-  );
-};
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button text="good" handleClick={increment(good, setGood)} />
-      <Button text="neutral" handleClick={increment(neutral, setNeutral)} />
-      <Button text="bad" handleClick={increment(bad, setBad)} />
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <div>{anecdotes[selected]}</div>
+      <div>
+        <button onClick={() => setSelected(getRandomInt(anecdotes.length))}>
+          {" "}
+          show random anecdote{" "}
+        </button>
+      </div>
     </div>
   );
 };
