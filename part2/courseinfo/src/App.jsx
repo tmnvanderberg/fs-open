541d03a6b2f0ffcfd79f281f829da44365217fbe
Timon van der Berg <tmnvanderberg@gmail.com>
2023-10-14 22:31:30 +0200

    b
    Signed-off-by: Timon van der Berg <tmnvanderberg@gmail.com>

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,4 @@
-const Part = ({ part }) =>{
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    parts.map(
-      (part) =>
-        <Part part={part} key={part.name} />
-    )
-  )
-}
-
-const Sum = ({parts}) => {
-  const total = parts.reduce((previous, current) => previous + current.exercises, 0)
-  return (
-    <div> <u> Total </u>: {total} </div>
-  )
-}
-
-const Header = ({name}) => {
-  return (
-    <h1> {name} </h1>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Sum parts={course.parts} />
-    </div>
-  )
-}
+import Course from "./components/course"
 
 const App = () => {
   const courses = [
