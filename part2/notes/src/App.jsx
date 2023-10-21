61c5394092aa813d4b84574564496e1b5d221681
Timon van der Berg <tmnvanderberg@gmail.com>
2023-10-21 18:37:24 +0200

    pre-exercise

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -28,9 +28,7 @@ const App = () => {
   useEffect(() => {
     noteService
       .getAll()
-      .then(initialNotes => {
-        setNotes(initialNotes)
-      })
+      .then(initialNotes => setNotes(initialNotes))
   }, [])
 
   const toggleImportance = (id) => {
@@ -41,32 +39,38 @@ const App = () => {
       .then(returnedNote => {
         setNotes(notes.map(note => note.id != id ? note : returnedNote))
       })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
-  return (
-    <div>
-      <h1>Notes</h1>
+    return (
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important': 'all'}
-        </button>
-      </div>
-      <ul>
-        { notesToShow.map(note =>
-          <Note key={note.id} note={note} toggleImportance={toggleImportance}/>
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
+        <h1>Notes</h1>
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all'}
+          </button>
+        </div>
+        <ul>
+          {notesToShow.map(note =>
+            <Note key={note.id} note={note} toggleImportance={toggleImportance} />
+          )}
+        </ul>
+        <form onSubmit={addNote}>
+          <input
+            value={newNote}
+            onChange={handleNoteChange}
           />
-        <button type="submit">save</button>
-      </form>
-    </div>
-  )
-}
+          <button type="submit">save</button>
+        </form>
+      </div>
+    )
+  }
 
-export default App
+  export default App
--- part2/notes/src/services/notes.jsx
+++ part2/notes/src/services/notes.jsx
@@ -2,23 +2,27 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = () => {
-    request = axios.get(baseUrl)
-    console.log(`request:${request}`)
-    return request.then(response => response.data)
+    const request = axios.get(baseUrl)
+    const nonExisting = {
+        id: 10000,
+        content: 'This note is not saved to server',
+        important: true,
+    }
+    return request.then(response => response.data.concat(nonExisting))
 }
 
 const create = newObject => {
-    request = axios.post(baseUrl, newObject)
+    const request = axios.post(baseUrl, newObject)
     return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-    request = axios.put(`${baseUrl}/${id}`, newObject)
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
 }
 
 export default {
-    getAll: getAll,
-    create: create,
-    update: update
+    getAll,
+    create,
+    update
 }
